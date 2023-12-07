2d73da6ce0139dbad81eb6ac5beb56b8b45f310b
jooligupta <jooligupta2000@gmail.com>
2023-12-07 11:34:34 +0530

    mongo

--- Controller/ContactController.js
+++ Controller/ContactController.js
@@ -14,7 +14,7 @@ class ContactController {
       await data.save();
       res.status(200).json({ msg: "Contact successfully created!..", data });
     } catch (err) {
-      res.status(500).json({ message: "Internal Server Error" + error });
+      res.status(500).json({ message: "Internal Server Error" + err });
     }
   };
   static getcontactus = async (req, res) => {
